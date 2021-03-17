681671204b9ecc88d3da290ff605d1f8298abc51
yamori-masato <yamorimasato@gmail.com>
2021-03-17 17:07:22 +0900

    表示のバリエーションを増やす

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,34 @@
 import React, { FC } from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const StyledButton = styled.button`
-  background-color: dodgerblue;
-  border: none;
+const StyledButton = styled.button<{cancel: boolean}>`
   box-shadow: none;
-  color: white;
   font-size: 1rem;
   height: 2rem;
   min-width: 5rem;
   padding: 0 1rem;
   cursor: pointer;
+
+  ${props => props.cancel ? css`
+    color: gray;
+    background-color: white;
+    border: solid 1px gray; 
+  ` : css`
+    color: white;
+    background-color: dodgerblue;
+    border: none;
+  `}
 `
 
 interface Props {
   children: string
+  cancel?: boolean
   onClick: () => void
 }
 
 const Button: FC<Props> = (props) => {
   return (
-    <StyledButton onClick={props.onClick}>
+    <StyledButton onClick={props.onClick} cancel={!!props.cancel}>
       {props.children}
     </StyledButton>
   )

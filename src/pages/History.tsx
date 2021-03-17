8c983783505dd09fe60b06637bb50bb27717c680
yamori-masato <yamorimasato@gmail.com>
2021-03-17 20:20:31 +0900

    Historyコンポーネントを修正

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
-import Button from '../components/Button'
+import { Link } from 'react-router-dom'
 import Header from '../components/Header'
 import Main from '../components/Main'
 
 const History = () => {
-  const history = useHistory()
   return (
     <>
       <Header>
-        <Button onClick={() => history.push('/editor')}>
-          エディタへ戻る
-        </Button>
+        <Link to="/editor">エディタへ戻る</Link>
       </Header>
       <Main>
 
